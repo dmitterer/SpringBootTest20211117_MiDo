235d4a1ceb4c19b75840f8aef780f7553062ebb2
Dominik Mitterer <dmitterer@tsn.at>
2021-11-17 09:14:10 +0100

    Initial commit

---
+++ src/main/java/com/itkollegimsttest/SpringBootTest20211117/SpringBootTest20211117Application.java
@@ -0,0 +1,13 @@
+package com.itkollegimsttest.SpringBootTest20211117;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest20211117Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest20211117Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itkollegimsttest/SpringBootTest20211117/SpringBootTest20211117ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkollegimsttest.SpringBootTest20211117;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest20211117ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
